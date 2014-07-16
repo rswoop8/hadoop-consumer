97d45bf9dda4193dfec10236e5d71eba472bc229
rohanswaroop <nahor.swaroop@gmail.com>
2014-07-15 17:10:47 -0700

    Formatting

--- src/main/java/ConsumerGroupExample.java
+++ src/main/java/ConsumerGroupExample.java
@@ -10,65 +10,70 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
  
 public class ConsumerGroupExample {
-    private final ConsumerConnector consumer;
-    private final String topic;
-    private  ExecutorService executor;
+  private final ConsumerConnector consumer;
+  private final String topic;
+  private  ExecutorService executor;
  
-    public ConsumerGroupExample(String a_zookeeper, String a_groupId, String a_topic) {
-        consumer = kafka.consumer.Consumer.createJavaConsumerConnector(createConsumerConfig(a_zookeeper, a_groupId));
-        this.topic = a_topic;
-    }
+  public ConsumerGroupExample(
+    String a_zookeeper, String a_groupId, String a_topic) {
+    consumer = kafka.consumer.Consumer.createJavaConsumerConnector(
+    		createConsumerConfig(a_zookeeper, a_groupId));
+    this.topic = a_topic;
+  }
  
-    public void shutdown() {
-        if (consumer != null) consumer.shutdown();
-        if (executor != null) executor.shutdown();
-    }
+  public void shutdown() {
+    if (consumer != null) consumer.shutdown();
+    if (executor != null) executor.shutdown();
+  }
 
-    public void run(int a_numThreads) {
-        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
-        topicCountMap.put(topic, new Integer(a_numThreads));
-        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
-        List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
+  public void run(int a_numThreads) {
+    Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+    topicCountMap.put(topic, new Integer(a_numThreads));
+    Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap =
+      consumer.createMessageStreams(topicCountMap);
+    List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(topic);
  
-        // now launch all the threads
-        //
-        executor = Executors.newFixedThreadPool(a_numThreads);
+    // now launch all the threads
+    //
+    executor = Executors.newFixedThreadPool(a_numThreads);
  
-        // now create an object to consume the messages
-        //
-        int threadNumber = 0;
-        for (final KafkaStream stream : streams) {
-            executor.submit(new ConsumerTest(stream, threadNumber));
-            threadNumber++;
-        }
+    // now create an object to consume the messages
+    //
+    int threadNumber = 0;
+    for (final KafkaStream stream : streams) {
+      executor.submit(new ConsumerTest(stream, threadNumber));
+      threadNumber++;
     }
+  }
  
-    private static ConsumerConfig createConsumerConfig(String a_zookeeper, String a_groupId) {
-        Properties props = new Properties();
-        props.put("zookeeper.connect", a_zookeeper);
-        props.put("group.id", a_groupId);
-        props.put("zookeeper.session.timeout.ms", "400");
-        props.put("zookeeper.sync.time.ms", "200");
-        props.put("auto.commit.interval.ms", "1000");
+  private static ConsumerConfig createConsumerConfig(
+    String a_zookeeper, String a_groupId) {
+    Properties props = new Properties();
+    props.put("zookeeper.connect", a_zookeeper);
+    props.put("group.id", a_groupId);
+    props.put("zookeeper.session.timeout.ms", "400");
+    props.put("zookeeper.sync.time.ms", "200");
+    props.put("auto.commit.interval.ms", "1000");
  
-        return new ConsumerConfig(props);
+    return new ConsumerConfig(props);
+  }
+ 
+  public static void main(String[] args) {
+    String zooKeeper = args[0];
+    String groupId = args[1];
+    String topic = args[2];
+    int threads = Integer.parseInt(args[3]);
+ 
+    ConsumerGroupExample example = new ConsumerGroupExample(
+      zooKeeper, groupId, topic);
+    example.run(threads);
+ 
+    try {
+      Thread.sleep(10000);
     }
+    catch (InterruptedException ie) {
  
-    public static void main(String[] args) {
-        String zooKeeper = args[0];
-        String groupId = args[1];
-        String topic = args[2];
-        int threads = Integer.parseInt(args[3]);
- 
-        ConsumerGroupExample example = new ConsumerGroupExample(zooKeeper, groupId, topic);
-        example.run(threads);
- 
-        try {
-            Thread.sleep(10000);
-        }
-        catch (InterruptedException ie) {
- 
-        }
-        example.shutdown();
     }
+    example.shutdown();
+  }
 }
--- src/main/java/RunConsumer.java
+++ src/main/java/RunConsumer.java
@@ -1,9 +1,10 @@
 import java.util.*;
 public class RunConsumer {
-    public static String topic;
-    public static void main(String[] args) {
-    	topic = args[2];
-        ConsumerGroupExample consumer = new ConsumerGroupExample(args[0], args[1], args[2]);
-        consumer.run(1);
-    }
+  public static String topic;
+  public static void main(String[] args) {
+  	topic = args[2];
+    ConsumerGroupExample consumer = new ConsumerGroupExample(
+      args[0], args[1], args[2]);
+    consumer.run(1);
+  }
 }
